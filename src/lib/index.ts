af01837cc2db158fe180c0fae612edb3a042da6e
Dariusz Filipiak <darekf77@gmail.com>
2023-04-01 02:29:47 +0200

    update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 //#region @browser
 export * from './websql-control-panel/websql-control-panel.component';
 export * from './websql-control-panel/websql-control-panel.module';
+export * from './material.module';
 //#endregion
 export * from './my-standalone-lib';
