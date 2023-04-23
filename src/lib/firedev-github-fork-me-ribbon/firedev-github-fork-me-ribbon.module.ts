7fc773c3bcb093c9746c39b610082338abd9d0e2
Dariusz Filipiak <darekf77@gmail.com>
2023-04-23 06:39:14 +0200

    update

---
+++ src/lib/firedev-github-fork-me-ribbon/firedev-github-fork-me-ribbon.module.ts
@@ -0,0 +1,14 @@
+//#region @browser
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FiredevGithubForkMeRibbonComponent } from './firedev-github-fork-me-ribbon.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [FiredevGithubForkMeRibbonComponent],
+  exports: [FiredevGithubForkMeRibbonComponent],
+})
+export class FiredevGithubForkMeRibbonModule { }
+//#endregion
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,8 @@
 export * from './firedev-file';
 export * from './firedev-github-fork-me-corner';
+export * from './firedev-github-fork-me-ribbon';
 export * from './firedev-websql-control-panel';
 //#region @browser
 export * from './firedev-full-material.module';
 //#endregion
+export * from './firedev.models';
