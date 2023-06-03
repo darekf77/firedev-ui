9cbf1da09c62b49b9449a1073f1db3d7d93e9327
Dariusz Filipiak <dariusz.filipiak@igt.com>
2023-06-03 22:23:53 +0200

    update

--- src/lib/firedev-ui-helpers/firedev-ui-helpers.ts
+++ src/lib/firedev-ui-helpers/firedev-ui-helpers.ts
@@ -97,6 +97,11 @@ export namespace FiredevUIHelpers {
     return response.data;
   }
 
+  export function numValue(pixelsCss: string) {
+    // tslint:disable-next-line:radix
+    return parseInt(pixelsCss.replace('px', ''));
+  }
+
 
 }
 
