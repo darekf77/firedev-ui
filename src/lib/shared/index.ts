4fe94142e34f685348c34f3f80dc75c65903a10e
Dariusz Filipiak <darekf77@gmail.com>
2024-02-04 01:42:31 +0100

    update

--- src/lib/shared/index.ts
+++ src/lib/shared/index.ts
@@ -3,4 +3,5 @@
 export { FiredevResizeService } from './firedev-resize.service';
 export { FiredevLongPress } from './firedev-long-press.directive';
 export { FiredevInjectHTMLDirective } from './firedev-inject-html.directive';
+export { SafePipe } from './safe.pipe';
 //#endregion
